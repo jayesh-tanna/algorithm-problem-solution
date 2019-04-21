52b08276923582c59dcfd828cfc25573775f175a
Jayesh Tanna <tanna.jay90@gmail.com>
2019-04-21 09:08:11 +0530

    Lisa's Work Book
    Lisa's Work Book

---
+++ Algorithms/Implementation/LisaWorkBook.cs
@@ -0,0 +1,19 @@
+//Problem: https://www.hackerrank.com/challenges/lisa-workbook/problem
+//Language: C#
+
+static int workbook(int n, int k, int[] arr) {
+        int specialProblem = 0;
+            int pageNo = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j <= arr[i]; j++)
+                {
+                    if (pageNo == j)
+                        specialProblem++;
+
+                    if (j % k == 0 || j == arr[i])
+                        pageNo++;
+                }
+            }
+            return specialProblem;
+    }
