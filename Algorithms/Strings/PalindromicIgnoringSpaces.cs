97d3df5bcd6f3c994aa2943a7a4598b7e5aa35fc
Jayesh Tanna <tanna.jay90@gmail.com>
2019-07-26 21:53:34 +0530

    Check if palindromic ignoring spaces

---
+++ Algorithms/Strings/PalindromicIgnoringSpaces.cs
@@ -0,0 +1,35 @@
+/*
+Given a string, check if it is a palindrome by ignoring spaces. E.g. race car would be a palindrome.
+https://practice.geeksforgeeks.org/problems/string-palindromic-ignoring-spaces/0Check if palindromic ignoring spaces
+*/
+
+using System;
+public class GFG {
+	static public void Main () {
+		int t = Convert.ToInt32(Console.ReadLine());
+		while(t-- > 0){
+		    string str = Console.ReadLine();
+		    string output = Palindromic(str);
+		    Console.WriteLine(output);
+		}
+	}
+	
+	private static string Palindromic(string str){
+	    int l = 0;
+	    int r = str.Length - 1;
+	    while(l <= r){
+	        
+	        while(str[l] == ' ')
+	            l++;
+	            
+	        while(str[r] == ' ')
+	            r--;
+	        
+	        if(str[l] != str[r])
+                return "NO";
+            l++;
+            r--;
+	    }
+	    return "YES";
+	}
+}
