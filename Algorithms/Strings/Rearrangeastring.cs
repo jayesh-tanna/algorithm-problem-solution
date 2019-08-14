8c7bb8e0786578d74c47c25ff2bbf6b99ddbb9ae
Jayesh Tanna <tanna.jay90@gmail.com>
2019-08-14 17:46:26 +0530

    Rearrange a string

---
+++ Algorithms/Strings/Rearrangeastring.cs
@@ -0,0 +1,44 @@
+/*
+Given a string containing uppercase alphabets and integer digits (from 0 to 9), 
+the task is to print the alphabets in the order followed by the sum of digits.
+https://practice.geeksforgeeks.org/problems/rearrange-a-string/0
+Example:
+Input:
+2
+AC2BEW3
+ACCBA10D2EW30
+
+Output:
+ABCEW5
+AABCCDEW6
+*/
+
+
+
+using System;
+using System.Text;
+using System.Linq;
+public class GFG {
+	static public void Main () {
+		int t = Convert.ToInt32(Console.ReadLine());
+		while(t-- > 0){
+		    string s = Console.ReadLine();
+		    Console.WriteLine(ReArrangeString(s));
+		}
+	}
+	
+	private static string ReArrangeString(string s)
+	{
+	  int sum = 0;
+	  StringBuilder sb = new StringBuilder();
+	  for(int i=0;i<s.Length;i++)
+	  {
+	      if(char.IsLetter(s[i]))
+	        sb.Append(s[i]);
+	       else if(char.IsDigit(s[i]))
+	        sum += Convert.ToInt32(char.GetNumericValue(s[i]));
+	  }
+	  var array = sb.ToString().OrderBy(x => x).ToArray();
+	  return  new string(array) + sum;
+	}
+}
