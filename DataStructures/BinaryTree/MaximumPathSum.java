e3582ec42d759004605c41def907baabe0b7ea0c
Jayesh Tanna <tanna.jay90@gmail.com>
2019-09-29 07:30:34 +0530

    Max path sum

---
+++ DataStructures/BinaryTree/MaximumPathSum.java
@@ -0,0 +1,29 @@
+/*
+https://practice.geeksforgeeks.org/problems/maximum-path-sum/1
+Given a binary tree in which each node element contains a number. Find the maximum possible sum from one leaf node to another.
+*/
+
+class SumUtil
+{
+    int sum;
+}
+class GfG
+{
+    public static int maxPathSum(Node root)
+    {
+        SumUtil res = new SumUtil();
+        maxPath(root,res);
+        return res.sum;
+    }
+    public static int maxPath(Node root,SumUtil res)
+    {
+        if(root == null)
+            return 0;
+        if(root.left == null && root.right == null)
+            return root.data;
+        int ls = maxPath(root.left,res);
+        int rs = maxPath(root.right,res);
+        res.sum = Math.max(res.sum, ls + rs + root.data);
+        return Math.max(ls,rs) + root.data;
+    }
+}Max
