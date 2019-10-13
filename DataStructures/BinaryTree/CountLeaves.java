0716d6c23a3353ae5a3586bdcaec55dd28fbf082
Jayesh Tanna <tanna.jay90@gmail.com>
2019-10-13 07:05:20 +0530

    Pre order

--- DataStructures/BinaryTree/CountLeaves.java
+++ DataStructures/BinaryTree/CountLeaves.java
@@ -46,3 +46,16 @@ class GfG
         return sum;
     }
 }
+//Preorder
+class GfG
+{
+    int countLeaves(Node node) 
+    {
+        if(node == null)
+            return 0;
+        if(node.left == null && node.right == null)
+            return 1;
+        int sum = countLeaves(node.left) + countLeaves(node.right);
+        return sum;
+    }
+}
