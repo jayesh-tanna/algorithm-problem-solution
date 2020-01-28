4a76951a297b234024a58ded8c167eebf4905708
Jayesh Tanna <tanna.jay90@gmail.com>
2020-01-28 20:59:37 +0530

    Find duplicates from array

--- CodingInterviewQuestions/4.FindDuplicates.cs
+++ CodingInterviewQuestions/4.FindDuplicates.cs
@@ -1,1 +1,24 @@
 
+/*
+Write a function to find duplicates from given integer array.
+*/
+
+
+static List<int> FindDuplicates(int[] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException();
+            if (arr.Length == 0)
+                throw new ArgumentException();
+            HashSet<int> set = new HashSet<int>();
+            List<int> duplicateItems = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (set.Contains(arr[i]))
+                    duplicateItems.Add(arr[i]);
+                else
+                    set.Add(arr[i]);
+            }
+            duplicateItems.Sort();
+            return duplicateItems;
+        }
