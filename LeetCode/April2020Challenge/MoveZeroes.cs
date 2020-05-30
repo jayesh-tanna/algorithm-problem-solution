0d8a1110c23eb1ea942912b2e937019a389465fc
Jayesh Tanna <tanna.jay90@gmail.com>
2020-05-30 08:23:34 +0530

    added url

--- LeetCode/April2020Challenge/MoveZeroes.cs
+++ LeetCode/April2020Challenge/MoveZeroes.cs
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3286/
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 Example:
 
