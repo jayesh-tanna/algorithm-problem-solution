23bc18269329f556f7a9d0563e1c49635786210c
Jayesh Tanna <tanna.jay90@gmail.com>
2020-06-05 17:20:46 +0530

    Improved complexity by adding condition

--- Algorithms/Sorting/BubbleSort.cs
+++ Algorithms/Sorting/BubbleSort.cs
@@ -19,15 +19,19 @@ internal class Program
         {
             for (int i = 0; i < nums.Length - 1; i++)
             {
+                bool swap = false;
                 for (int j = 0; j < nums.Length - i - 1; j++)
                 {
                     if (nums[j] > nums[j + 1])
                     {
+                        swap = true;
                         int temp = nums[j];
                         nums[j] = nums[j + 1];
                         nums[j + 1] = temp;
                     }
                 }
+                if (!swap)
+                    break;
             }
         }
  }
